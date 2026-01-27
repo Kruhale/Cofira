7429cb506cac86b914e0347adad64e18e239b09a
Alejandro-Bravo2 <abravoc@iesrafaelalberti.es>
2026-01-27 20:31:47 +0100

    fix: update Angular dependencies and adjust HTML structure
    - Updated Angular dependency imports in various files to ensure correct loading order.
    - Removed obsolete JSON configuration for Chrome DevTools.
    - Adjusted metadata for Angular packages to reflect new file hashes.
    - Modified index.html to include a data attribute for the HTML tag and updated stylesheet and script references.
    - Enhanced SCSS for the alimentacion component to support dark theme styling.

--- cofira/.angular/cache/20.3.12/cofira/vite/deps/@angular_platform-browser.js
+++ cofira/.angular/cache/20.3.12/cofira/vite/deps/@angular_platform-browser.js
@@ -41,8 +41,8 @@ import {
 } from "./chunk-BVCONOPW.js";
 import "./chunk-BV6YITCM.js";
 import "./chunk-RKHO7A5K.js";
-import "./chunk-HWYXSU2G.js";
 import "./chunk-JRFR6BLO.js";
+import "./chunk-HWYXSU2G.js";
 import "./chunk-MARUHEWW.js";
 import "./chunk-GOMI4DH3.js";
 export {
