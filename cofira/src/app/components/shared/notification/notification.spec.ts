7aa241c8a0ca351d374aef58f04a29661f239087
Kruhale <alejandrobravocalderom@gmail.com>
2026-07-30 01:22:55 +0200

    feat(ui): notification stack and motion accents

---
+++ cofira/src/app/components/shared/notification/notification.spec.ts
@@ -0,0 +1,24 @@
+import {provideZonelessChangeDetection} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {Notification} from './notification';
+
+describe('Notification', () => {
+  let component: Notification;
+  let fixture: ComponentFixture<Notification>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Notification],
+      providers: [provideZonelessChangeDetection()],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Notification);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
